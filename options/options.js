e9739aae48e87bbe93094e3e4d74d45a2481006c
Crowdbotics <team@crowdbotics.com>
2023-08-02 22:30:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sima-test-42594",
+  url: "https://sima-test-42594.botics.co",
+  api: "https://sima-test-42594.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
